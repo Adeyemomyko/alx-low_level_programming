80b12a89bbfb0fbab1acf620b0ab74c1a8cdcc8e
Adeyemomyko <adeyemomichael68@gmail.com>
2023-11-30 01:04:42 -0800

    Firstj

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdargs.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...){
+
+	va_list app;
+	unsigned int i, sum = 0;
+
+	va_start (app, n)
+		if n== 0,
+			return 0;
+	for (i=0, i<n, i++){
+		sum += va_args(app,int)
+	}
+	va_end(app);
+	
+	return (sum);
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+Documentation is a must in ALX Software engineering.
+
+I will update the use of each file when the project is done
+
+thanks!!! :-)
